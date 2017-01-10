a4368fa29f23f3b5aa378e1da01d6265e7e349ee
Vineeta Sharma <vineetasharma18791@gmail.com>
2017-01-10 15:51:54 +0530

    Day separator Added.

--- src/Day.js
+++ src/Day.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
+    StyleSheet,
+    Text,
+    View,
 } from 'react-native';
 
 import moment from 'moment/min/moment-with-locales.min';
@@ -10,70 +10,96 @@ import moment from 'moment/min/moment-with-locales.min';
 import { isSameDay, isSameUser, warnDeprecated } from './utils';
 
 export default class Day extends React.Component {
-  render() {
-    if (!isSameDay(this.props.currentMessage, this.props.previousMessage)) {
-      return (
-        <View style={[styles.container, this.props.containerStyle]}>
-          <View style={[styles.wrapper, this.props.wrapperStyle]}>
-            <Text style={[styles.text, this.props.textStyle]}>
-              {moment(this.props.currentMessage.createdAt).locale(this.context.getLocale()).format('ll').toUpperCase()}
-            </Text>
-          </View>
-        </View>
-      );
+    getDay(time) {
+        const day=moment(time).locale(this.context.getLocale()).format('ll').toUpperCase();
+        const today=moment(new Date()).locale(this.context.getLocale()).format('ll').toUpperCase();
+        const yesterday=moment(new Date()).day(0).locale(this.context.getLocale()).format('ll').toUpperCase();
+        if(day==today){
+            return 'Today';
+        }
+        if(day==yesterday){
+            return 'Yesterday';
+        }
+        return day;
+    };
+    render() {
+        if (!isSameDay(this.props.currentMessage, this.props.previousMessage)) {
+            return (
+                <View style={[styles.container, this.props.containerStyle]}>
+                  <View style={[styles.wrapper]}>
+                    <View style={styles.horizontalLine} />
+                  </View>
+                  <View style={[styles.wrapper]}>
+                    <Text style={[styles.text, this.props.textStyle]}>
+                        {this.getDay(this.props.currentMessage.createdAt)}
+                    </Text>
+                  </View>
+                  <View style={[styles.wrapper, this.props.wrapperStyle]}>
+                    <View style={styles.horizontalLine} />
+                  </View>
+                </View>
+            );
+        }
+        return null;
     }
-    return null;
-  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 5,
-    marginBottom: 10,
-  },
-  wrapper: {
-    // backgroundColor: '#ccc',
-    // borderRadius: 10,
-    // paddingLeft: 10,
-    // paddingRight: 10,
-    // paddingTop: 5,
-    // paddingBottom: 5,
-  },
-  text: {
-    backgroundColor: 'transparent',
-    color: '#b2b2b2',
-    fontSize: 12,
-    fontWeight: '600',
-  },
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 5,
+        marginBottom: 10,
+        flexDirection: "row",
+    },
+    horizontalLine:{
+        backgroundColor: '#9f9fa3',
+        height:2,
+    },
+    wrapper: {
+        flex:1,
+        justifyContent: 'center',
+        // backgroundColor: '#ccc',
+        // borderRadius: 10,
+        // paddingLeft: 10,
+        // paddingRight: 10,
+        // paddingTop: 5,
+        // paddingBottom: 5,
+    },
+    text: {
+        backgroundColor: 'transparent',
+        color: '#a3a3a3',
+        fontSize: 12,
+        fontWeight: '600',
+        textAlign:'center',
+    },
 });
 
 Day.contextTypes = {
-  getLocale: React.PropTypes.func,
+    getLocale: React.PropTypes.func,
 };
 
 Day.defaultProps = {
-  currentMessage: {
-    // TODO test if crash when createdAt === null
-    createdAt: null,
-  },
-  previousMessage: {},
-  containerStyle: {},
-  wrapperStyle: {},
-  textStyle: {},
-  //TODO: remove in next major release
-  isSameDay: warnDeprecated(isSameDay),
-  isSameUser: warnDeprecated(isSameUser),
+    currentMessage: {
+        // TODO test if crash when createdAt === null
+        createdAt: null,
+    },
+    previousMessage: {},
+    containerStyle: {},
+    wrapperStyle: {},
+    textStyle: {},
+    //TODO: remove in next major release
+    isSameDay: warnDeprecated(isSameDay),
+    isSameUser: warnDeprecated(isSameUser),
 };
 
 Day.propTypes = {
-  currentMessage: React.PropTypes.object,
-  previousMessage: React.PropTypes.object,
-  containerStyle: View.propTypes.style,
-  wrapperStyle: View.propTypes.style,
-  textStyle: Text.propTypes.style,
-  //TODO: remove in next major release
-  isSameDay: React.PropTypes.func,
-  isSameUser: React.PropTypes.func,
+    currentMessage: React.PropTypes.object,
+    previousMessage: React.PropTypes.object,
+    containerStyle: View.propTypes.style,
+    wrapperStyle: View.propTypes.style,
+    textStyle: Text.propTypes.style,
+    //TODO: remove in next major release
+    isSameDay: React.PropTypes.func,
+    isSameUser: React.PropTypes.func,
 };
